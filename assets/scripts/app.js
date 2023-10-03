260d3675087e71d26136bf0ff7704ec88a721a8c
Michael Neely <michael@michaelneely.net>
2023-10-03 10:19:00 -0400

    Adding the second argument to the AddeveneHandler for the switchBtn and more functonality to switch back and forth between active and finished projects

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,8 +3,9 @@ class Tooltip {
 }
 
 class ProjectItem {
-    constructor(id) {
+    constructor(id, updateProjectListFunction) {
         this.id = id;
+        this.updateProjectListHandler = updateProjectListFunction;
         this.connectMoreInfoButton();
         this. connectSwitchButton();
     }
@@ -16,27 +17,34 @@ class ProjectItem {
     connectSwitchButton() {
         const projectItemElement = document.getElementById(this.id);
         const switchBtn = projectItemElement.querySelector('button:last-of-type');
-        swtichBtn.addEventListener('click', )
+        switchBtn.addEventListener('click', this.updateProjectListHandler)
     }
 }
 
 class ProjectList {
     projects = [];
     constructor(type) {
+        this.type = type;
+
         const prjItems = document.querySelectorAll(`#${type}-projects li`);
         for(const prjItem of prjItems) {
-            this.projects.push(new ProjectItem(prjItem.id));
+            this.projects.push(new ProjectItem(prjItem.id, this.switchProject.bind(this)));
         }
         console.log(this.projects);
     }
 
-    addProject() {
+    setSwitchHandler(switchHandlerFunction) {
+        this.switchHandler = switchHandlerFunction;
+    }
 
+    addProject() {
+    console.log(this);
     }
 
     switchProject(projectId) {
         // const projectIndex = this.projects.findIndex(p => p.id === projectId);
         // this.projects.splice(projectLIst, 1);
+        this.switchHandler(this.projects.find(p => p.id === projectId))
         this.projects = this.projects.filter(p => p.id !== projectId);
     }
 }
@@ -45,6 +53,8 @@ class App {
     static init() {
         const activeProjectList = new ProjectList('active');
         const finishedProjectList = new ProjectList('finished');
+        activeProjectList.setSwitchHandler(finishedProjectList.addProject.bind(finishedProjectList));
+        finishedProjectList.setSwitchHandler(activeProjectList.addProject.bind(activeProjectList));
     }
 }
 
